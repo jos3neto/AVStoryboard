b42f7082b90584bfda103420c07de73468d781aa
jos3barros <barros_j@yahoo.com>
2018-01-18 09:34:55 -0200

    Initial Commit

---
+++ AVStoryboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVStoryboard
+//
+//  Created by Jose on 18/1/18.
+//  Copyright © 2018 Jose. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AVStoryboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVStoryboard
+//
+//  Created by Jose on 18/1/18.
+//  Copyright © 2018 Jose. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
